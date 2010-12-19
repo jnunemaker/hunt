536ef7e2808a66ff33b49d00f11b33bd27713581
John Nunemaker <nunemaker@gmail.com>
2010-12-19 15:21:17 -0500

    Beefed up utilities. Added Util.to_stemmed_words.

--- lib/hunt/util.rb
+++ lib/hunt/util.rb
@@ -1,7 +1,34 @@
 module Hunt
   module Util
+    Separator = ' '
+    StripPunctuationRegex = /[^a-zA-Z0-9]/
+    PunctuationReplacement = ''
+    WordsToIgnore = [
+      "about", "above", "after", "again", "against", "all",
+      "and", "any", "are", "aren't", "because", "been",
+      "before", "being", "below", "between", "both", "but", "cannot",
+      "can't", "could", "couldn't", "did", "didn't", "does", "doesn't",
+      "doing", "don't", "down", "during", "each", "few", "for", "from",
+      "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having",
+      "he'd", "he'll", "her", "here", "here's", "hers", "herself", "he's",
+      "him", "himself", "his", "how", "how's", "i'd", "if", "i'll", "i'm",
+      "into", "isn't", "its", "it's", "itself", "i've", "let's",
+      "more", "most", "mustn't", "myself", "nor", "not",
+      "off", "once", "only", "other", "ought", "our", "ours", "ourselves",
+      "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's",
+      "should", "shouldn't", "some", "such", "than", "that", "that's", "the",
+      "their", "theirs", "them", "themselves", "then", "there", "there's", "these",
+      "they", "they'd", "they'll", "they're", "they've", "this", "those", "through",
+      "too", "under", "until", "up", "very", "was", "wasn't", "we'd",
+      "we'll", "were", "we're", "weren't", "we've", "what", "what's", "when",
+      "when's", "where", "where's",  "which", "while", "who", "whom", "who's",
+      "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd",
+      "you'll", "your", "you're", "yours", "yourself", "yourselves", "you've",
+      "the", "how"
+    ]
+
     def strip_puncuation(value)
-      value.to_s.gsub(/[^a-zA-Z0-9]/, '')
+      value.to_s.gsub(StripPunctuationRegex, PunctuationReplacement)
     end
 
     def stem(word)
@@ -10,10 +37,19 @@ module Hunt
 
     def to_words(value)
       value.
-        squeeze(' ').
-        split(' ').
-        map     { |word| strip_puncuation(word.downcase) }.
-        reject  { |word| word.blank? }
+        to_s.
+        squeeze(Separator).
+        split(Separator).
+        map     { |word| word.downcase }.
+        reject  { |word| word.size < 3 }.
+        reject  { |word| WordsToIgnore.include?(word) }.
+        map     { |word| strip_puncuation(word) }.
+        reject  { |word| word.blank? }.
+        uniq
+    end
+
+    def to_stemmed_words(value)
+      to_words(value).map { |word| stem(word) }
     end
 
     extend self
--- spec/hunt/util_spec.rb
+++ spec/hunt/util_spec.rb
@@ -16,24 +16,47 @@ describe Hunt::Util do
   end
 
   describe ".to_words" do
+    it "does not fail with nil" do
+      Hunt::Util.to_words(nil).should == []
+    end
+
     it "converts string to array of words" do
-      Hunt::Util.to_words('this is my sentence').should == %w(this is my sentence)
+      Hunt::Util.to_words('first sentence').should == %w(first sentence)
     end
 
     it "squeezes multiple spaces" do
-      Hunt::Util.to_words('this is my  sentence').should == %w(this is my sentence)
+      Hunt::Util.to_words('first    sentence').should == %w(first sentence)
     end
 
     it "removes punctuation" do
-      Hunt::Util.to_words('my first sentence.').should == %w(my first sentence)
+      Hunt::Util.to_words('woot!').should == %w(woot)
     end
 
     it "removes blanks from removed punctuation" do
-      Hunt::Util.to_words('my first sentence & second').should == %w(my first sentence second)
+      Hunt::Util.to_words('first sentence & second').should == %w(first sentence second)
     end
 
     it "lowercases each word" do
-      Hunt::Util.to_words('My First Sentence').should == %w(my first sentence)
+      Hunt::Util.to_words('Sweet First Sentence').should == %w(sweet first sentence)
+    end
+
+    it "removes any words under 3 characters" do
+      Hunt::Util.to_words('my first sentence').should == %w(first sentence)
+    end
+
+    it "removes words that should be ignored" do
+      Hunt::Util.to_words('how was your day').should == %w(day)
+      Hunt::Util.to_words("didn't you see that").should == %w(see)
+    end
+
+    it "removes duplicates" do
+      Hunt::Util.to_words('boom boom').should == %w(boom)
+    end
+  end
+
+  describe ".to_stemmed_words" do
+    it "converts value to array of stemmed words" do
+      Hunt::Util.to_stemmed_words('I just Caught you kissing.').should == %w(just caught kiss)
     end
   end
 end
